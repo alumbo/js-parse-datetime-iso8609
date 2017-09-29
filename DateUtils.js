7996ed4baa210cef5b557a2fac9fd009ff8c242f
Matthieu Chavigny <matttieu@gmail.com>
2017-09-29 12:25:42 +0200

    Update DateUtils.js

--- DateUtils.js
+++ DateUtils.js
@@ -3,13 +3,21 @@ class DateUtils {
   static parseDateISO8609(dateStr) {
     let date = new Date(dateStr);
     // In case of browser was not able to parse timezone
+    let timezoneMinutes = 0;
     if (date.toString() === 'Invalid Date') {
       date = new Date(dateStr.substr(0, dateStr.length - 5));
+      let timezoneStr = dateStr.substr(-5);
+      let timezoneSign = timezoneStr.substr(0, 1);
+      let timezoneHours = parseInt(timezoneStr.substr(1, 2));
+      timezoneMinutes = parseInt(timezoneStr.substr(3, 2));
+      timezoneMinutes += timezoneHours * 60;
+      timezoneMinutes = parseInt(timezoneSign + timezoneMinutes);
     }
     // In case of browser did not impact timezone offset on date
     if (dateStr.indexOf(date.toString().substr(16, 8)) !== -1) {
-      date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+      timezoneMinutes -= date.getTimezoneOffset();
     }
+    date.setMinutes(date.getMinutes() - timezoneMinutes);
     return date;
   }
   
